74162c749e47c67ab0a62ff5ad5648bcda301987
Pavel Parvanov <pparvanov@2create.studio>
2025-02-15 09:42:44 +0200

    Install and configure Inertia.js with React JS

---
+++ resources/js/app.jsx
@@ -0,0 +1,17 @@
+/**
+ * External dependencies.
+ */
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.jsx`,
+            import.meta.glob("./pages/**/*.jsx")
+        ),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
