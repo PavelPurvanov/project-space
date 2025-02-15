a47bf8e5d4631bcfac0aec6052720aa5ef1648b1
Pavel Parvanov <pparvanov@2create.studio>
2025-02-15 13:43:01 +0200

    Format App.jsx code

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,12 @@
 /**
  * External dependencies.
  */
-import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.jsx`,
-            import.meta.glob("./pages/**/*.jsx")
-        ),
+    resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
