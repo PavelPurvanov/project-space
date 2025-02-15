289da7f7be5be2ec66c548fc5d626b5dc5c6c47b
Pavel Parvanov <pparvanov@2create.studio>
2025-02-15 14:33:09 +0200

    Update sidebar items

--- resources/js/components/sidebar/sidebar.jsx
+++ resources/js/components/sidebar/sidebar.jsx
@@ -12,16 +12,16 @@ const Sidebar = () => (
     <div className="sidebar">
         <HStack>
             <button type="button" className="sidebar__item">
-                <FolderOpenDot />
+                <FolderOpenDot color="#0c3d51" />
             </button>
             <button type="button" className="sidebar__item">
-                <Users />
+                <Users color="#0c3d51" />
             </button>
         </HStack>
 
         <HStack>
             <button type="button" className="sidebar__item">
-                <Settings />
+                <Settings color="#0c3d51" />
             </button>
         </HStack>
     </div>
