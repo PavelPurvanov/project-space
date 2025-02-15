d2db303133c2590b3eef7bdb3a9d11bd7de276be
Pavel Parvanov <pparvanov@2create.studio>
2025-02-15 13:45:07 +0200

    Add app.scss

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import react from "@vitejs/plugin-react";
-import laravel from "laravel-vite-plugin";
-import { defineConfig } from "vite";
+/**
+ * External dependencies.
+ */
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            '@css': path.resolve(__dirname, './resources/css'),
+        },
+    },
     plugins: [
         react(),
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
