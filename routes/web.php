c37de3927da2170c3ccd416313890bbb84762683
Pavel Parvanov <pparvanov@2create.studio>
2025-02-18 20:48:31 +0200

    Add web route for Home page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(ProjectController::class)->group(function () {
-    Route::get('/projects', 'index')->name('project.index');
+Route::middleware('guest')->group(function () {
+    Route::inertia('/', 'home')->name('home');
+});
+
 });
