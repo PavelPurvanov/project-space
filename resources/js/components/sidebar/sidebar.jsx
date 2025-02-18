5c50faa6c844b2fb20dcc3129773ca6af19954da
Pavel Parvanov <pparvanov@2create.studio>
2025-02-18 20:40:43 +0200

    Use vertical stacks in Sidebar

--- resources/js/components/sidebar/sidebar.jsx
+++ resources/js/components/sidebar/sidebar.jsx
@@ -7,27 +7,34 @@ import { Users, Settings, FolderOpenDot } from 'lucide-react';
 /**
  * Internal dependencies.
  */
-import { HStack } from '@/components/stack/stack';
+import { VStack } from '@/components/stack/stack';
 
 const Sidebar = () => {
     const { url } = usePage();
 
     return (
         <div className="sidebar">
-            <HStack>
-                <Link href="/projects" className={url === '/projects' ? 'sidebar__item active' : 'sidebar__item'}>
+            <VStack>
+                <Link
+                    href="/projects"
+                    className={
+                        url === '/projects'
+                            ? 'sidebar__item active'
+                            : 'sidebar__item'
+                    }
+                >
                     <FolderOpenDot color="#0c3d51" />
                 </Link>
                 <Link className="sidebar__item">
                     <Users color="#0c3d51" />
                 </Link>
-            </HStack>
+            </VStack>
 
-            <HStack>
+            <VStack>
                 <Link className="sidebar__item">
                     <Settings color="#0c3d51" />
                 </Link>
-            </HStack>
+            </VStack>
         </div>
     );
 };
