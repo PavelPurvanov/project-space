3ed77cfb9e53a99dc8a5561c60de6a666152345e
Pavel Parvanov <pparvanov@2create.studio>
2025-02-18 20:49:33 +0200

    Add web route for Projects page

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ Route::middleware('guest')->group(function () {
     Route::inertia('/', 'home')->name('home');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::controller(ProjectController::class)->group(function () {
+        Route::get('/projects', 'index')->name('project.index');
+    });
 });
