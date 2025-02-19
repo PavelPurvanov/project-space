39a7da4ce07d7e4af6bafe612a2c46b7b1612f94
Pavel Parvanov <pparvanov@2create.studio>
2025-02-19 20:40:24 +0200

    Add LoginController web routes group

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::inertia('/', 'home')->name('home');
+
+    Route::controller(LoginController::class)->group(function () {
+        Route::post('/login', 'login')->name('login');
+        Route::post('/logout', 'logout')->name('logout');
+        Route::inertia('/login', 'login')->name('login.edit');
+    });
 });
 
 Route::middleware('auth')->group(function () {
