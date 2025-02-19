5399d811314233a2d3270935953eacb53811587b
Pavel Parvanov <pparvanov@2create.studio>
2025-02-19 20:32:50 +0200

    Show built-in Inertia progress bar

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,7 +6,16 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
+    progress: {
+        delay: 1,
+        color: '#e26040',
+        includeCSS: true,
+    },
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.jsx`,
+            import.meta.glob('./pages/**/*.jsx')
+        ),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
