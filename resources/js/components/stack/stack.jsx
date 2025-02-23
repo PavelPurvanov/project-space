de2f293c631b99b52064436e93d8b0cd16f7697b
Pavel Parvanov <pparvanov@2create.studio>
2025-02-23 12:25:50 +0200

    Update Stack: add bordered variant, add align modifier

--- resources/js/components/stack/stack.jsx
+++ resources/js/components/stack/stack.jsx
@@ -3,10 +3,12 @@
  */
 import classNames from 'classnames';
 
-const Stack = ({ gap, justify, children, direction }) => (
+const Stack = ({ gap, align, justify, variant, children, direction }) => (
     <div
         className={classNames('stack', `stack--direction-${direction}`, {
+            [`stack--align-${align}`]: align,
             [`stack--spacing-${gap}`]: gap,
+            [`stack--variant-${variant}`]: variant,
             [`stack--justify-${justify}`]: justify,
         })}
     >
@@ -14,14 +16,26 @@ const Stack = ({ gap, justify, children, direction }) => (
     </div>
 );
 
-export const VStack = ({ gap, justify, children }) => (
-    <Stack gap={gap} justify={justify} direction="column">
+export const VStack = ({ gap, align, variant, justify, children }) => (
+    <Stack
+        gap={gap}
+        align={align}
+        variant={variant}
+        justify={justify}
+        direction="column"
+    >
         {children}
     </Stack>
 );
 
-export const HStack = ({ gap, justify, children }) => (
-    <Stack gap={gap} justify={justify} direction="row">
+export const HStack = ({ gap, align, variant, justify, children }) => (
+    <Stack
+        gap={gap}
+        align={align}
+        variant={variant}
+        justify={justify}
+        direction="row"
+    >
         {children}
     </Stack>
 );
