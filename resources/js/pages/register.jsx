a82b9c94abdda1cdc9da9fb4fd73ffa0d1a89ea3
Pavel Parvanov <pparvanov@2create.studio>
2025-03-01 08:49:55 +0200

    Add Register page

---
+++ resources/js/pages/register.jsx
@@ -0,0 +1,96 @@
+/**
+ * External dependencies.
+ */
+import { Rocket } from 'lucide-react';
+
+/**
+ * Internal dependencies.
+ */
+import Text from '@/components/text/text';
+import Input from '@/components/input/input';
+import Button from '@/components/button/button';
+import Layout from '@/components/layout/layout';
+import useForm from '@/hooks/use-form';
+import Stack, { VStack, HStack } from '@/components/stack/stack';
+
+const Register = () => {
+    const { post, errors, processing, handleOnChange } = useForm({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        post('/register');
+    };
+
+    return (
+        <Layout guest>
+            <Stack gap={40} variant="bordered" direction="column">
+                <Text as="h2" size="30" align="center">
+                    <HStack gap={4} align="center" justify="center">
+                        Register <Rocket size={32} />
+                    </HStack>
+                </Text>
+
+                <form onSubmit={handleOnSubmit}>
+                    <VStack gap={32}>
+                        <VStack gap={8}>
+                            <Input
+                                name="name"
+                                error={errors.name}
+                                onChange={handleOnChange}
+                                placeholder="Name"
+                            />
+
+                            <Input
+                                name="email"
+                                type="email"
+                                error={errors.email}
+                                onChange={handleOnChange}
+                                placeholder="Email"
+                            />
+
+                            <Input
+                                name="password"
+                                type="password"
+                                error={errors.password}
+                                onChange={handleOnChange}
+                                placeholder="Password"
+                            />
+
+                            <Input
+                                name="password_confirmation"
+                                type="password"
+                                error={errors.password_confirmation}
+                                onChange={handleOnChange}
+                                placeholder="Confirm password"
+                            />
+                        </VStack>
+
+                        <Button
+                            type="submit"
+                            color="orange"
+                            modifier="medium"
+                            processing={processing}
+                        >
+                            Register
+                        </Button>
+                    </VStack>
+                </form>
+
+                <hr />
+
+                <Text align="center">
+                    Already have an account?{' '}
+                    <Text as="a" href="/login">
+                        Login
+                    </Text>
+                </Text>
+            </Stack>
+        </Layout>
+    );
+};
+
+export default Register;
